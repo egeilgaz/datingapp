e9831cc22518c9949175680a973aef4f0607c598
egeilgaz <ilgaz-ege@hotmail.com>
2019-10-07 18:50:24 +0300

    initial Commit

---
+++ datinapp.API/models/Value.cs
@@ -0,0 +1,12 @@
+namespace datinapp.API.models
+{
+    public class Value
+    {
+        public int Id {
+            get; set; 
+        }
+        public string Name {
+            get ; set;
+        }
+    }
+}
